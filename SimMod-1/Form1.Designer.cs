edf057bce38de3f0ef7dbafc2b4186d9cfd03916
Nexp1e <nexp1e@stud.tsu.ru>
2022-02-15 11:20:02 +0700

    Version for presentation. Updated app layout and added calculations.

--- SimMod-1/Form1.Designer.cs
+++ SimMod-1/Form1.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.labelInputHeight = new System.Windows.Forms.Label();
             this.inputHeight = new System.Windows.Forms.NumericUpDown();
@@ -41,31 +42,36 @@
             this.labelInputSize = new System.Windows.Forms.Label();
             this.inputWeight = new System.Windows.Forms.NumericUpDown();
             this.labelInputWeight = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.calculatedDataGrid = new System.Windows.Forms.DataGridView();
             this.buttonCalculate = new System.Windows.Forms.Button();
-            this.buttonResetValues = new System.Windows.Forms.Button();
-            this.buttonClearGraphs = new System.Windows.Forms.Button();
-            this.buttonClearData = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.inputTimeStep = new System.Windows.Forms.NumericUpDown();
+            this.labelInputTimeStep = new System.Windows.Forms.Label();
+            this.timeStep = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.distance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maxHeightColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.speedColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputHeight)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputAngle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputVelocity)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputWeight)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.calculatedDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTimeStep)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
             this.chart1.Location = new System.Drawing.Point(13, 93);
             this.chart1.Name = "chart1";
-            series1.BorderWidth = 3;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(1079, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -78,7 +84,6 @@
             this.labelInputHeight.Size = new System.Drawing.Size(38, 13);
             this.labelInputHeight.TabIndex = 2;
             this.labelInputHeight.Text = "Height";
-            this.labelInputHeight.Click += new System.EventHandler(this.label1_Click);
             // 
             // inputHeight
             // 
@@ -107,7 +112,6 @@
             this.labelInputAngle.Size = new System.Drawing.Size(34, 13);
             this.labelInputAngle.TabIndex = 4;
             this.labelInputAngle.Text = "Angle";
-            this.labelInputAngle.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // inputVelocity
             // 
@@ -172,13 +176,19 @@
             this.labelInputWeight.TabIndex = 10;
             this.labelInputWeight.Text = "Weight";
             // 
-            // dataGridView1
+            // calculatedDataGrid
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 399);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1079, 190);
-            this.dataGridView1.TabIndex = 12;
+            this.calculatedDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.calculatedDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.timeStep,
+            this.distance,
+            this.maxHeightColumn,
+            this.speedColumn});
+            this.calculatedDataGrid.Location = new System.Drawing.Point(13, 399);
+            this.calculatedDataGrid.Name = "calculatedDataGrid";
+            this.calculatedDataGrid.RowHeadersWidth = 4;
+            this.calculatedDataGrid.Size = new System.Drawing.Size(1079, 190);
+            this.calculatedDataGrid.TabIndex = 12;
             // 
             // buttonCalculate
             // 
@@ -188,44 +198,65 @@
             this.buttonCalculate.TabIndex = 13;
             this.buttonCalculate.Text = "Calculate";
             this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
             // 
-            // buttonResetValues
+            // timer1
             // 
-            this.buttonResetValues.Location = new System.Drawing.Point(888, 12);
-            this.buttonResetValues.Name = "buttonResetValues";
-            this.buttonResetValues.Size = new System.Drawing.Size(99, 75);
-            this.buttonResetValues.TabIndex = 14;
-            this.buttonResetValues.Text = "Reset values";
-            this.buttonResetValues.UseVisualStyleBackColor = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // buttonClearGraphs
+            // inputTimeStep
             // 
-            this.buttonClearGraphs.Location = new System.Drawing.Point(783, 12);
-            this.buttonClearGraphs.Name = "buttonClearGraphs";
-            this.buttonClearGraphs.Size = new System.Drawing.Size(99, 75);
-            this.buttonClearGraphs.TabIndex = 15;
-            this.buttonClearGraphs.Text = "Clear graphs";
-            this.buttonClearGraphs.UseVisualStyleBackColor = true;
+            this.inputTimeStep.DecimalPlaces = 4;
+            this.inputTimeStep.Location = new System.Drawing.Point(281, 69);
+            this.inputTimeStep.Name = "inputTimeStep";
+            this.inputTimeStep.Size = new System.Drawing.Size(120, 20);
+            this.inputTimeStep.TabIndex = 15;
+            this.inputTimeStep.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
-            // buttonClearData
+            // labelInputTimeStep
             // 
-            this.buttonClearData.Location = new System.Drawing.Point(678, 12);
-            this.buttonClearData.Name = "buttonClearData";
-            this.buttonClearData.Size = new System.Drawing.Size(99, 75);
-            this.buttonClearData.TabIndex = 16;
-            this.buttonClearData.Text = "Clear calculated data";
-            this.buttonClearData.UseVisualStyleBackColor = true;
+            this.labelInputTimeStep.AutoSize = true;
+            this.labelInputTimeStep.Location = new System.Drawing.Point(237, 71);
+            this.labelInputTimeStep.Name = "labelInputTimeStep";
+            this.labelInputTimeStep.Size = new System.Drawing.Size(29, 13);
+            this.labelInputTimeStep.TabIndex = 14;
+            this.labelInputTimeStep.Text = "Step";
+            this.labelInputTimeStep.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timeStep
+            // 
+            this.timeStep.HeaderText = "Time step";
+            this.timeStep.Name = "timeStep";
+            // 
+            // distance
+            // 
+            this.distance.HeaderText = "Distance";
+            this.distance.Name = "distance";
+            // 
+            // maxHeightColumn
+            // 
+            this.maxHeightColumn.HeaderText = "Max height";
+            this.maxHeightColumn.Name = "maxHeightColumn";
+            // 
+            // speedColumn
+            // 
+            this.speedColumn.HeaderText = "Speed at the end point";
+            this.speedColumn.Name = "speedColumn";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1104, 601);
-            this.Controls.Add(this.buttonClearData);
-            this.Controls.Add(this.buttonClearGraphs);
-            this.Controls.Add(this.buttonResetValues);
+            this.Controls.Add(this.inputTimeStep);
+            this.Controls.Add(this.labelInputTimeStep);
             this.Controls.Add(this.buttonCalculate);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.calculatedDataGrid);
             this.Controls.Add(this.inputWeight);
             this.Controls.Add(this.labelInputWeight);
             this.Controls.Add(this.inputSize);
@@ -245,7 +276,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.inputVelocity)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.inputWeight)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.calculatedDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputTimeStep)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -264,11 +296,15 @@
         private System.Windows.Forms.Label labelInputSize;
         private System.Windows.Forms.NumericUpDown inputWeight;
         private System.Windows.Forms.Label labelInputWeight;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView calculatedDataGrid;
         private System.Windows.Forms.Button buttonCalculate;
-        private System.Windows.Forms.Button buttonResetValues;
-        private System.Windows.Forms.Button buttonClearGraphs;
-        private System.Windows.Forms.Button buttonClearData;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown inputTimeStep;
+        private System.Windows.Forms.Label labelInputTimeStep;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timeStep;
+        private System.Windows.Forms.DataGridViewTextBoxColumn distance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maxHeightColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn speedColumn;
     }
 }
 
--- SimMod-1/Form1.cs
+++ SimMod-1/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Configuration;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,24 +14,96 @@ namespace SimMod_1
 {
     public partial class Form1 : Form
     {
+        const decimal g = 9.81M;
+        const decimal C = 0.15M;
+        const decimal rho = 1.29M;
+
+
+        decimal t, x, y, v0, cosa, sina, S, m, k, vx, vy;
+        decimal dt = 0.01M;
+        decimal maxHeight, distanceTravelled, speedAtEndPoint;
+
+        Series curSeries;
+        int seriesCount = 0;
+
         public Form1()
         {
             InitializeComponent();
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.Series[0].Points.Clear();
+
+            inputTimeStep.Value = 0.01M;
+            inputTimeStep.Increment = 0.001M;
+
+            calculatedDataGrid.Columns[0].Name = "Time step";
+            calculatedDataGrid.Columns[1].Name = "Distance";
+            calculatedDataGrid.Columns[2].Name = "Max height";
+            calculatedDataGrid.Columns[3].Name = "Speed at the end point";
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void buttonCalculate_Click(object sender, EventArgs e)
         {
+            if (!timer1.Enabled)
+            {
+                curSeries = new Series();
+                curSeries.ChartType = SeriesChartType.Line;
+                curSeries.BorderWidth = 3;
+                seriesCount++;
+                chart1.Series.Add(curSeries);
+
+                maxHeight = 0;
+                distanceTravelled = 0;
+                speedAtEndPoint = 0;
+
+                t = 0;
+                dt = inputTimeStep.Value;
+                x = 0;
+                y = inputHeight.Value;
+                v0 = inputVelocity.Value;
+                double a = (double)inputAngle.Value * Math.PI / 180;
+                cosa = (decimal)Math.Cos(a);
+                sina = (decimal)Math.Sin(a);
+                S = inputSize.Value;
+                m = inputWeight.Value;
+                k = 0.5M * C * rho * S / m;
+                vx = v0 * cosa;
+                vy = v0 * sina;
+
+                chart1.Series[seriesCount - 1].Points.AddXY(x, y);
+
+                timer1.Start();
+            }
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
+            decimal v = (decimal)Math.Sqrt((double)(vx * vx + vy * vy));
+            if (y >= maxHeight)
+            {
+                maxHeight = y;
+            }
+            speedAtEndPoint = v;
+            distanceTravelled = x;
+            t += dt;
+            vx = vx - k * vx * v * dt;
+            vy = vy - (g + k * vy * v) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
+            chart1.Series[seriesCount - 1].Points.AddXY(x, y);
+            if (y <= 0)
+            {
+                timer1.Stop();
+                AddDataToTable(dt, maxHeight, distanceTravelled, speedAtEndPoint);
+            }
 
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
+        private void AddDataToTable(decimal timeStep, decimal distance, decimal height, decimal speed)
         {
-
+            string[] newRow = { timeStep.ToString(), distance.ToString(), height.ToString(), speed.ToString() };
+            calculatedDataGrid.Rows.Add(newRow);
         }
     }
 }
